2fc7d760689928f7648f2e8d86fde340734b037b
Alexander Horch <malexhorch@gmail.com>
2021-12-07 13:51:24 +0100

    2016 d10 part-1 prepped

---
+++ src/y2016/template.kt
@@ -0,0 +1,21 @@
+package y2016
+
+const val day = "01"
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("day$day\\Day${day}_test")
+    check(part1(testInput) == 1)
+//    check(part2(testInput) == 1)
+
+    val input = readInput("day$day\\Day$day")
+    println(part1(input))
+//    println(part2(input))
+}
